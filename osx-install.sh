89528ce49ad2ee615c60d345b14bffd1c0176d4d
Alexander <alexander@bjrns-imac.home>
2020-04-13 22:56:17 +0200

    test of wich shell

--- osx-install.sh
+++ osx-install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -e
+echo "python3"
 which python3
 ./osx-make-iconset.sh
 
-python py2app_setup.py install
-python py2app_setup.py py2app
+python3 py2app_setup.py install
+python3 py2app_setup.py py2app
 
 INSTALL_PATH="/Applications/nbopen.app"
 if [ -e $INSTALL_PATH ]; then
